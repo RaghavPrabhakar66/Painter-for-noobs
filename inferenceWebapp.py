ac34d5deb869f700415edbfc4d478c447eea71ed
Raghav Prabhakar <raghavprabhakar66@gmail.com>
2020-10-06 00:43:07 +0530

    Added custom style option and type of model to be used.

--- inferenceWebapp.py
+++ inferenceWebapp.py
@@ -14,26 +14,37 @@ stylePath = {
     'Kadinsky - Several Circles': r'Images\Style\Kandinsky-Several Circles.png',
     'Haring - Dance'            : r'Images\Style\Haring-Dance.png',
     'Picasso - Weeping Woman'   : r'Images\Style\picasso-weeping woman.png',
-    'Vangogh - Whitefield'      : r'Images\Style\Vangogh-whitefield.png'
+    'Vangogh - Whitefield'      : r'Images\Style\Vangogh-whitefield.png',
+    'Custom Style'              : r'Images\Style\Custom Style.jpg'
 }
 
 if __name__ == '__main__':
+    e = None
     st.title('Painter for Noobs')
 
     st.sidebar.header('Configurations')
+    method = st.sidebar.selectbox( 'Select a Method', ['Neural Style Transfer - Traditional', 'Fast style Transfer', 'General Adverserial Networks'])
 
-    option = st.sidebar.selectbox( 'Select a Paint Style', ['Vangogh - Starry Night', 'Kadinsky - Several Circles', 'Haring - Dance', 'Picasso - Weeping Woman', 'Vangogh - Whitefield'])
-    showStyleImage = st.sidebar.checkbox("Want to see the image", key='1')
+    if method == 'Neural Style Transfer - Traditional':
+        option = st.sidebar.selectbox( 'Select a Paint Style', ['Vangogh - Starry Night', 'Kadinsky - Several Circles', 'Haring - Dance', 'Picasso - Weeping Woman', 'Vangogh - Whitefield', 'Custom Style'])
+        if option=='Custom Style':
+            style_uploaded_file = st.sidebar.file_uploader("Choose an image file", type=["png", 'jpg', 'jpeg', 'tiff'], key="2")
 
-#    epochs  = st.sidebar.slider('Epochs', 0, 1000, 300)
-    stylize = st.sidebar.slider('Stylize Weight')
-    showStatus = st.sidebar.button('Submit')
+            if style_uploaded_file is not None:
+                im = Image.open(style_uploaded_file).convert('RGB')
+                im.save(r'Images\Style\Custom Style.jpg', quality=95)
+            
+        showStyleImage = st.sidebar.checkbox("Want to see the image", key='1')
 
-    if showStyleImage:
-        style = Image.open(stylePath[option]).convert('RGB')
-        st.sidebar.image(style, caption='Style Image', use_column_width=True)
+        e  = st.sidebar.slider('Epochs', 0, 1000, 300)
+        stylize = st.sidebar.slider('Stylize Weight')
+        showStatus = st.sidebar.button('Submit')
 
-    content_uploaded_file = st.file_uploader("Choose a image file", type=["png", 'jpg', 'jpeg', 'tiff'], key="1")
+        if showStyleImage:
+            style = Image.open(stylePath[option]).convert('RGB')
+            st.sidebar.image(style, caption='Style Image', use_column_width=True)
+
+    content_uploaded_file = st.file_uploader("Choose an image file", type=["png", 'jpg', 'jpeg', 'tiff'], key="1")
     showContentImage = st.checkbox("Want to see the image", key='2')
 
     if content_uploaded_file is not None:
@@ -46,16 +57,14 @@ if __name__ == '__main__':
 
     if clicked:
         startTime = time.time()
-        myProgressBar = st.progress(0)
         device = ('cuda:0' if torch.cuda.is_available() else 'cpu')
-        print(device)
 
         model = models.vgg19(pretrained=True).features
 
         for param in model.parameters():
             param.requires_grad = False
 
-        model.to('cuda:0')
+        model.to(device)
 
         contentImg, styleImg = readImgs(r'serverUpload\testContent.jpg', os.path.abspath(stylePath[option]))
 
@@ -76,12 +85,13 @@ if __name__ == '__main__':
         content_wt = 100
         style_wt = 1e8
 
-        epochs = 300
+        epochs = e
 
-        target = contentImg.clone().requires_grad_(True).to()
+        target = contentImg.clone().requires_grad_(True).to(device)
 
         optimizer = torch.optim.Adam([target],lr=0.007)
-
+        rem = int(epochs/100)
+        myProgressBar = st.progress(0)
         for i in range(epochs):
             targetFeatures  = modelActivation(target, model)
             contentLoss = torch.mean((contentFeatures['conv4_2'] - targetFeatures['conv4_2'])**2)
@@ -96,19 +106,20 @@ if __name__ == '__main__':
 
             totalLoss = styleLoss + contentLoss
 
-            print("epoch : {} Total Loss : {}".format(i, totalLoss))
+            #print("epoch : {} Total Loss : {}".format(i, totalLoss))
 
             optimizer.zero_grad()
             totalLoss.backward()
             optimizer.step()
 
-            if i%3==0:
-                myProgressBar.progress(int(i/3))
+            if i%rem==0:
+                myProgressBar.progress(int(i/rem))
 
         trainStyle = convertFromTensor(target)
         utils.save_image(target, 'generated1.png')
 
         endTime = time.time()
+        myProgressBar.empty()
         im = Image.open('generated1.png').convert('RGB')
         st.image(im, caption='Converted Image')
         st.write('Time Taken : ', (endTime-startTime), ' s')
